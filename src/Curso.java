c5005b1002eb00dfb9bb59cfa9862058f397ca81
Antonio Mendoza <antonio.mendoza@gmail.com>
2023-07-19 13:36:10 +0200

    otra forma de construir alumno

--- src/Curso.java
+++ src/Curso.java
@@ -11,11 +11,13 @@ public class Curso {
     public Curso(){
         notaMedia = -1;
         alumnos = new Alumno[NUMERO_ALUMNOS];
+         for (int n=0; n<NUMERO_ALUMNOS;n++){
+            alumnos[n] = new Alumno();
+        }
     }
 
     public void alta(){
         for (int n=0; n<NUMERO_ALUMNOS;n++){
-            alumnos[n] = new Alumno();
             alumnos[n].alta();
         }
     }
